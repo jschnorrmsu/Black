0ea0a247c79bbf3dc3082f698df54eb940000897
jschnorrmsu <jschnorr@msudenver.edu>
2019-06-13 08:54:31 -0600

    Changed exit status to be printed/returned from main.c, changed waitpid function to include WIFEXITED and WEXITSTATUS

--- counter.c
+++ counter.c
@@ -2,18 +2,18 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <assert.h>
+#include <errno.h>
+#include <sys/types.h>
 // Contributors: Jake Schnorr, Luke Smith. 
 
 int main(int argc, char*argv[]) {
 
     char* endPointer;
-    int childPID = getpid();
+    pid_t childPID = getpid();
     int parentPID = getppid();
     long int countToThis = strtol(argv[1], &endPointer, 10);
+    assert(errno == 0);
 
-    // Parent/Child PID should always return > 0 
-    assert(parentPID > 0);
-    assert(childPID > 0);
     assert(printf("Child PID: %d \nParent PID: %d\n", childPID, parentPID) != 0);
 
     if(argc <= 1) {
@@ -26,6 +26,6 @@ int main(int argc, char*argv[]) {
         assert(printf("Process ID:  %d %d \n", childPID, i) != 0);
     
     }
-    assert(printf("Process %d exited with status: %ld \n", childPID, countToThis) != 0);
-    return countToThis;   
+
+    return countToThis;
 }
--- main.c
+++ main.c
@@ -10,21 +10,28 @@
 int main() {
 
     int exitStatus;
-    // Create a child process, capture the child process ID
+    // Create a child process, capture the child process ID on sucess.
     pid_t childPID = fork();
 
     if(childPID < 0) {
-        perror("System call <fork()> failed\n");
+        perror("System call fork() failed\n");
         exit(-1);
     }
 
     else if(childPID == 0) {
-        // Execute counter.c, capture program status
+        // Execute counter.c, capture program return status.
         exitStatus = execl("./counter", "counter", "5", NULL);
+        perror("Function execl() failed\n");
+        exit(exitStatus);
     }
 
     else {
+        // Wait for child process to end, print exit status returned from counter.c upon sucess.
         assert(waitpid(childPID, &exitStatus, 0) != -1);
+        if(WIFEXITED(exitStatus)) {
+            assert(printf("Process exited with status: %d \n", WEXITSTATUS(exitStatus)) != 0);
+        }
+
     }
     return 0;
 }
