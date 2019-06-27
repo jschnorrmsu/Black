33a2612133c90368f62f189751f8dd68db6979ef
jschnorrmsu <jschnorr@msudenver.edu>
2019-06-27 08:25:13 -0600

    Put some meat on them bones, added signal hdnler and begun implementing project

--- Project Three/child.c
+++ Project Three/child.c
@@ -1,20 +1,14 @@
 #define _POSIX_C_SOURCE 200809l
 #include <unistd.h>
-#include <assert.h>
-#include <signal.h>
 #include <sys/types.h>
-#include <sys/wait.h>
-#include <error.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
 
-    pid_t parentPID = getppid();
     pid_t childPID = getpid();
-    //assert(kill(parentPID, SIGCHLD) != -1);
     while(1) {
-        printf("Awake in %d\n", childPID);
+        write("Awake in %d\n", childPID);
+        sleep(1);
     }
 
     return(0);
--- Project Three/main.c
+++ Project Three/main.c
@@ -17,8 +17,31 @@ int main() {
     sigemptyset (&action.sa_mask);
     action.sa_flags = SA_RESTART | SA_NOCLDSTOP;
 
+    //call pause at the end of main so that the arent doesnt 
+    // process diesnt exit from there. 
+
 }
 
 void handler(int signum) {
+
+    pid_t parentPID = getppid();
+    // SIGCHLD handler.
+    if(signum == 17) {
+        for(int i = 0; i <= 4; i ++) {
+            write(1, "Parent with ID# %d is going to SIGSTOP the child.", parentPID);
+            //SIGSTOP the child
+            sleep(2);
+
+            write(1, "Parent with ID# %d is going to SIGCONT the child", parentPID);
+            //SIGCONT the child
+            sleep(2);
+        }
+
+    }
+
+    // SIGINT handler. 
+    if(signum == 2) {
+        //handle SIGINT here. 
+    }
 
 }
